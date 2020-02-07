7270ae1003d03b1702997eef50b326223756ad72
Jeremiah Lam <jiamialam@gmail.com>
2020-02-07 11:31:02 -0800

    modified hosts so admin is not required. website_block also writes to hosts

--- website_block.py
+++ website_block.py
@@ -1,10 +1,13 @@
 import time
 from datetime import datetime as dt
-hosts_path = r'C:\Users\jiami\Desktop\Projects\Website_Blocker' #list of distracting site
+hosts_path = r'hosts' #list of distracting site
 redirect = "127.0.0.1" # redirect to different 
 website_list = ['www.facebook.com', 'facebook.com']
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, hour = 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, hour = 16):
         print('working hours are 8-4')
+        with open(hosts_path, 'w') as f:
+            for website in website_list:
+                f.write(website + '\n')
     time.sleep(300)
