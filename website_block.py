b988ccee9c0f90c132fbaee270d2ec13251a1b5f
Jeremiah Lam <jiamialam@gmail.com>
2020-02-07 12:05:15 -0800

    added actual host file (oops)

--- website_block.py
+++ website_block.py
@@ -1,13 +1,14 @@
 import time
 from datetime import datetime as dt
-hosts_path = r'hosts' #list of distracting site
+hosts_temp = r'hosts'
+hosts_path = r'C:\Windows\System32\drivers\etc\hosts' #list of distracting site
 redirect = "127.0.0.1" # redirect to different 
 website_list = ['www.facebook.com', 'facebook.com']
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, hour = 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, hour = 16):
         print('Work is equppied from 8-4')
-        with open(hosts_path, 'r+') as f:
+        with open(hosts_temp, 'r+') as f:
             content = f.read()
             for website in website_list:
                 if website in content:
