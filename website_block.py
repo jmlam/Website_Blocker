4cd773a630caf8e31a3c935a835bc866c60883b7
Jeremiah Lam <jiamialam@gmail.com>
2020-02-07 09:56:50 -0800

    add time checker

--- website_block.py
+++ website_block.py
@@ -1,3 +1,9 @@
+import time
 hosts_path = r'C:\Users\jiami\Desktop\Projects\Website_Blocker' #list of distracting site
 redirect = "127.0.0.1" # redirect to different 
 website_list = ['www.facebook.com', 'facebook.com']
+
+while True:
+    now = time.strftime("%a, %d %b %Y %H:%M:%S +0000")
+    print(now)#checktime
+    time.sleep(300)
