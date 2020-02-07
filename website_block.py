dd7e5bf73ec991939a9aa9668dbe9ca01b44dd29
Jeremiah Lam <jiamialam@gmail.com>
2020-02-07 12:36:18 -0800

    changed test host_temp to host_path

--- website_block.py
+++ website_block.py
@@ -8,7 +8,7 @@ website_list = ['www.facebook.com', 'facebook.com']
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, hour = 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, hour = 16):
         print('Work is equppied from 8-4')
-        with open(hosts_temp, 'r+') as f:
+        with open(hosts_path, 'r+') as f:
             content = f.read()
             for website in website_list:
                 if website in content:
@@ -17,11 +17,12 @@ while True:
                     f.write(redirect + '\t'+ website + '\n')
     else:
         print('Work is not equipped')
-        with open(hosts_temp, 'r+') as f:
+        with open(hosts_path, 'r+') as f:
             content = f.readlines() #read contents
             f.seek(0) #put pointer at top of file
             for line in content:
                 if not any(website in line for website in website_list):#any returns True if any website is in line any line of the content
                     f.write(line) #write contents of file without website
             f.truncate() #delete everything below
+    
     time.sleep(60)
