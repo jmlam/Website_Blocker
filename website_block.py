0438aee0352caba5d8f8b439219140ffd222b596
Jeremiah Lam <jiamialam@gmail.com>
2020-02-07 11:22:07 -0800

    added time checker between 8-4

--- website_block.py
+++ website_block.py
@@ -1,9 +1,10 @@
 import time
+from datetime import datetime as dt
 hosts_path = r'C:\Users\jiami\Desktop\Projects\Website_Blocker' #list of distracting site
 redirect = "127.0.0.1" # redirect to different 
 website_list = ['www.facebook.com', 'facebook.com']
 
 while True:
-    now = time.strftime("%a, %d %b %Y %H:%M:%S +0000")
-    print(now)#checktime
+    if dt(dt.now().year, dt.now().month, dt.now().day, hour = 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, hour = 16):
+        print('working hours are 8-4')
     time.sleep(300)
