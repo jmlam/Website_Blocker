40749aa597fa7baade70f1e5cd197a442729d1b5
Jeremiah Lam <jiamialam@gmail.com>
2020-02-07 09:48:42 -0800

    added initial framework for website blocker. a host file that will contain all unwanted sites

---
+++ website_block.py
@@ -0,0 +1,3 @@
+hosts_path = r'C:\Users\jiami\Desktop\Projects\Website_Blocker' #list of distracting site
+redirect = "127.0.0.1" # redirect to different 
+website_list = ['www.facebook.com', 'facebook.com']
